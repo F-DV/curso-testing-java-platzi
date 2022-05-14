df0af86b8cd45cf581db40d947ee8d6ca2c8afb6
Felipe <fquiceno1@misena.edu.co>
2022-05-13 21:17:14 -0500

    Forma de organizar los tests con JUnit

--- src/main/java/com/platzi/javatests/util/StringUtil.java
+++ src/main/java/com/platzi/javatests/util/StringUtil.java
@@ -3,6 +3,9 @@ package com.platzi.javatests.util;
 public class StringUtil {
 
     public static String repeat(String str, int times){
+        if(times <0){
+            throw new IllegalArgumentException("Negative times no allowed");
+        }
         String result = "";
         for (int i = 0; i < times; i++) {
             result += str;
--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -4,8 +4,26 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class StringUtilTest {
+    /*
+        - Normalmente realizamos una funcion para cada prueba,
+        - Especificamos que test estamos realizando en el nombre de la funcion
+        - Para un caso de error como por ejemplo negativo en este caso, debemos lanzar la exception en el caso de uso
+            y capturarla aqui en los tests
+     */
     @Test
-    public void testRepeat(){
+    public void repeat_string_once(){
+        Assert.assertEquals("hola",StringUtil.repeat("hola",1));
+    }
+    @Test
+    public void repeat_string_multiple_times(){
         Assert.assertEquals("holaholahola",StringUtil.repeat("hola",3));
+    }
+    @Test
+    public void repeat_string_cero_times(){
+        Assert.assertEquals("",StringUtil.repeat("hola",0));
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times(){
+        Assert.assertEquals("",StringUtil.repeat("hola",-1));
     }
 }
